ba83a89392de49523609d8a4e901cff8105dd50f
Ozzy-P <97415717+Ozzy-P@users.noreply.github.com>
2023-10-21 00:42:01 -0700

    Update Shared.lua
    Add generic key value pairs.

--- Shared.lua
+++ Shared.lua
@@ -3,7 +3,21 @@
 -- Assumes type of generic has a metamethod that returns a comparable value to functions (e.g. generic OOP)
 local heap = {}
 export type indexArray<E> = {
-	heap : {[number] : E}, 
+	heap : {
+		[number] : E
+	}, 
+	_HEAPSIZE : number
+}
+
+export type keyTable<E> = {
+	value: E,
+	key : number,
+}
+
+export type keyArray<E> = {
+	heap : {
+		keyTable<number>
+	},
 	_HEAPSIZE : number
 }
 
@@ -47,15 +61,40 @@ heap.MAX_HEAP_MAXIMUM = function(A:indexArray<number>)
 	if A._HEAPSIZE < 1 then
 		error("Invalid heap size for operation!");
 	end
-	return A[1];
+	return A.heap[1];
 end
 
 heap.MAX_HEAP_EXTRACT_MAX = function(A:indexArray<number>)
 	local MAXIMUM = heap.MAX_HEAP_MAXIMUM(A);
-	A[1] = A[A._HEAPSIZE];
+	A.heap[1] = A.heap[A._HEAPSIZE];
 	A._HEAPSIZE -= 1;
 	heap.MAX_HEAPIFY(A,1);
 	return MAXIMUM;
+end
+
+-- Must implement a comparable where x returns a value similar to heap array type.
+heap.MAX_HEAP_INCREASE_KEY = function(A:keyArray<number>, x:keyTable<number>, k:number)
+	if k < x.key then
+		error("MAX_HEAP_INCREASE may only increase the respective value of the object.");
+	end
+	x.key = k;
+	local i = nil;
+	do
+		for n = 1, A._HEAPSIZE do
+			if A[n].value == x.value then
+				i = n
+				break;
+			end
+		end
+	end
+	if(i == nil) then
+		error("Failed to find instance(s) of object X: ", x.value)
+	end
+	while i > 1 and A.heap[heap.PARENT(i)].key < A.heap[i].key do
+		-- TODO:
+		-- Do this, cause it's not explained, or will be explained in the near future.
+		i = heap.PARENT(i);
+	end
 end
 
 heap.HEAPSORT = function(A:indexArray<number>)
